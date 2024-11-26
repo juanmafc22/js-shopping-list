66e6152e758241d62bce3b95c0c2c121bd956c68
Juan Manuel Fernandez <juanmafc22@gmail.com>
2024-11-26 22:56:03 +0000

    Add delete functions to shopping list

--- 09-shopping-list/script.js
+++ 09-shopping-list/script.js
@@ -1,64 +1,66 @@
-const form = document.getElementById('item-form');
-const itemInput = document.getElementById('item-input');
-const itemUl = document.getElementById('itemUl');
-const clearBtn = document.getElementById('clear');
+const form = document.getElementById("item-form");
+const itemInput = document.getElementById("item-input");
+const itemUl = document.getElementById("itemUl");
+const clearBtn = document.getElementById("clear");
 
 // add item to the <ul></ul>
 function addItem(e) {
+  e.preventDefault();
 
-    e.preventDefault();
+  // get the item entered
+  const item = itemInput.value;
 
-    // get the item entered
-    const item = itemInput.value;
+  const li = document.createElement("li");
+  li.innerText = item;
 
-    const li = document.createElement('li');
-    li.innerText = item;
+  if (validateInput(item)) {
+    // create the icon
+    const icon = document.createElement("i");
+    icon.className = "fa-solid fa-xmark";
 
-    if (validateInput(item)) {
+    const button = document.createElement("button");
+    button.appendChild(icon);
+    button.className = "remove-item btn-link text-red";
 
-        // create the icon
-        const icon = document.createElement('i');
-        icon.className = 'fa-solid fa-xmark';
+    li.appendChild(button);
 
-        const button = document.createElement('button');
-        button.appendChild(icon);
-        button.className = 'remove-item btn-link text-red';
+    itemUl.appendChild(li);
 
-        li.appendChild(button);
-
-        itemUl.appendChild(li);
-
-        document.getElementById('item-input').value = "";
-
-    } else {
-
-        alert("Input field is empty");
-
-    }
+    document.getElementById("item-input").value = "";
+  } else {
+    alert("Input field is empty");
+  }
 }
 
-function clearAllLi() {
+function deleteAllLi() {
+  const allLi = document.querySelectorAll("li");
 
-    const allLi = document.querySelectorAll('li');
-
-    allLi.forEach(li => {
-        li.remove();
-    })
+  allLi.forEach((li) => {
+    li.remove();
+  });
 }
 
+// function to validate if the input is emtpy or not
 function validateInput(text) {
-
-    if (text != "") {
-        return true;
-    } else {
-        return false;
-    }
+  if (text != "") {
+    return true;
+  } else {
+    return false;
+  }
 }
 
-// clear all the <li></li> items from the <ul></ul>
+// function to delete individual <li></li>
+function deleteLi(e) {
+  if (e.target.classList.contains("fa-xmark")) {
+    e.target.parentElement.parentElement.remove();
+  }
+}
+
+// add click removal but through delegation, targeting the <ul></ul>
+itemUl.addEventListener("click", deleteLi);
 
 // add click to the clear all btn
-clearBtn.addEventListener('click', clearAllLi);
+clearBtn.addEventListener("click", deleteAllLi);
 
 // add submit event listener to the form
-form.addEventListener('submit', addItem);
+form.addEventListener("submit", addItem);
