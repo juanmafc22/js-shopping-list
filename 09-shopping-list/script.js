179055c3e4c8322102f75b47139f69989cbe5ace
Juan Manuel Fernández <juanmafc22@gmail.com>
2024-11-26 14:26:57 -0300

    Worked on 09-shopping list

--- 09-shopping-list/script.js
+++ 09-shopping-list/script.js
@@ -1,6 +1,7 @@
 const form = document.getElementById('item-form');
 const itemInput = document.getElementById('item-input');
 const itemUl = document.getElementById('itemUl');
+const clearBtn = document.getElementById('clear');
 
 // add item to the <ul></ul>
 function addItem(e) {
@@ -10,24 +11,54 @@ function addItem(e) {
     // get the item entered
     const item = itemInput.value;
 
-    // create the icon
-    const icon = document.createElement('i');
-    icon.className = 'fa-solid fa-xmark';
-
-    const button = document.createElement('button');
-    button.appendChild(icon);
-    button.className = 'remove-item btn-link';
-
     const li = document.createElement('li');
-    li.appendChild(button);
     li.innerText = item;
 
-    itemUl.appendChild(li);
+    if (validateInput(item)) {
 
-    document.getElementById('item-input').value = "";
+        // create the icon
+        const icon = document.createElement('i');
+        icon.className = 'fa-solid fa-xmark';
 
+        const button = document.createElement('button');
+        button.appendChild(icon);
+        button.className = 'remove-item btn-link text-red';
+
+        li.appendChild(button);
+
+        itemUl.appendChild(li);
+
+        document.getElementById('item-input').value = "";
+
+    } else {
+
+        alert("Input field is empty");
+
+    }
 }
 
+function clearAllLi() {
 
-// add item to the <ul></ul> on form submit
+    const allLi = document.querySelectorAll('li');
+
+    allLi.forEach(li => {
+        li.remove();
+    })
+}
+
+function validateInput(text) {
+
+    if (text != "") {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+// clear all the <li></li> items from the <ul></ul>
+
+// add click to the clear all btn
+clearBtn.addEventListener('click', clearAllLi);
+
+// add submit event listener to the form
 form.addEventListener('submit', addItem);
